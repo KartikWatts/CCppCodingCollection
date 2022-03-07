ec82a9a62116c3668cc423195bf5bd1b13fc4c82
KartikWatts <arorakartik4@gmail.com>
2022-03-07 19:55:47 +0530

    cached files cleaned git

--- __Build_Config_VS/testSolution.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-clear
-diff -bB __Build_Config_VS/output.txt __Build_Config_VS/solution.txt > __Build_Config_VS/difference.txt
-clear
-
-# echo Building... > output.txt && g++ -g ${relativeFile} -o output.out && clear && timeout 10 /usr/bin/time -v --output sys.txt ./output.out < input.txt > output.txt && clear && rm *out
